034c2470fffc7c6d369a806071599651bfb7b744
martinski74 <martin.dobr@abv.bg>
2022-12-08 18:38:04 +0200

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="app">
+  <h1>DropZone</h1>
+  <DropZoneVue @drop.prevent="drop" @change="selectedFile"/>
+  <span class="file-info">File: {{ dropzoneFile.name }}</span>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DropZoneVue from './components/DropZone.vue';
+import { ref } from "vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DropZoneVue,
+  },
+    setup() {
+      let dropzoneFile = ref("");
+
+      const drop = (e) => {
+        dropzoneFile.value = e.dataTransfer.files[0];
+      }
+
+      const selectedFile = () => {
+        dropzoneFile.value = document.querySelector('.dropzoneFile').files[0];
+      }
+      return {
+        dropzoneFile,
+        drop,
+        selectedFile
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.app {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f1f1f1;
+  h1 {
+    font-size: 40px;
+    margin-bottom: 32px;
+  }
+  .file-info {
+    margin-top: 32px;
+  }
 }
 </style>
